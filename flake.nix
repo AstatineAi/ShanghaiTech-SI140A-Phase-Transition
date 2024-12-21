c72efc5708925a6ad8d44c0c8b42e8bdeb9a3f30
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-21 18:30:33 +0800

    feat(flake): init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (python3.withPackages (
+            ps: with ps; [
+              jupyterlab
+              matplotlib
+              numpy
+            ]
+          ))
+        ];
+      };
+    };
+}
